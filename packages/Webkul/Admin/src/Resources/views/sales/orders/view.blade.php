39ea9c759a1e0861016d8ff667b0643b86b19170
burhangok <burhangok@yahoo.com>
2025-08-28 13:17:19 +0300

    fixed order view edit page

--- packages/Webkul/Admin/src/Resources/views/sales/orders/view.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sales/orders/view.blade.php
@@ -640,7 +640,7 @@ Kupon Satış Komisyonu
                             <div class="mt-3">
                                 <h6 class="font-weight-bold">Komisyon Açıklaması:</h6>
                                 <div class="p-3 bg-light rounded">
-                                    Sipariş #{{ $commission->order_id }} için {{ $commission->level }}. seviye komisyon (Sepet Tutarı: {{ core()->formatBasePrice($order->base_grand_total) }} Euro)
+                                    Sipariş #{{ $commission->order_id }} için {{ $commission->level }}. seviye komisyon (Sepet Tutarı: {{ core()->formatBasePrice($order->base_grand_total - $order->shipping_amount) }} Euro)
                                 </div>
                             </div>
                             @endif
